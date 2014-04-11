2ea8df130acf0df8b8a7bff4e724a0bc45ca052c
Nico Schlömer <nico.schloemer@gmail.com>
2014-04-11 15:20:06 +0200

    maintain trilinos backport patches

--- submit-trilinos.sh
+++ submit-trilinos.sh
@@ -8,9 +8,11 @@ THIS_DIR=`dirname $0`
 $THIS_DIR/launchpad-submitter \
   --name trilinos \
   --resubmission 1 \
-  --git-dir "$HOME/software/trilinos/dev/github/" \
-  --ubuntu-releases precise quantal saucy trusty \
+  --source-dir "$HOME/software/trilinos/dev/github/" \
+  --debian-dir "$HOME/rcs/debian-trilinos/debian/" \
+  --ubuntu-releases saucy trusty \
   --ppas nschloe/trilinos-nightly \
   --version-getter 'grep "Trilinos_VERSION " Version.cmake | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
-  --patches-dir patches-trilinos \
-  --submit
+  --extra-patches-dir patches-trilinos #\
+  #--submit
+  #--ubuntu-releases precise quantal saucy trusty \
