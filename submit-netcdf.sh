198787bf166c59c4db2de52ab0bda586946e9cd2
Nico Schlömer <nico.schloemer@gmail.com>
2014-05-22 01:08:16 +0200

    use new netcdf debian folder

--- submit-netcdf.sh
+++ submit-netcdf.sh
@@ -6,10 +6,10 @@ eval $(cat $HOME/.ssh/agent/info)
 THIS_DIR=`dirname $0`
 
 $THIS_DIR/launchpad-submitter \
-  --name netcdfc \
+  --name netcdf \
   --resubmission 1 \
   --source-dir "$HOME/software/netcdf-c/dev/source/" \
-  --debian-dir "$HOME/rcs/debian-netcdfc/" \
+  --debian-dir "$HOME/rcs/debian-netcdf-official/debian/" \
   --ubuntu-releases precise quantal saucy trusty \
   --ppas nschloe/netcdf-nightly nschloe/trilinos-nightly \
   --version-getter 'grep "^AC_INIT" configure.ac | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
