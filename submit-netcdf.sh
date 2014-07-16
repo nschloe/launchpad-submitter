5f3807c67b89aaf1afead408a0344a0fd1d42077
Nico Schlömer <nico.schloemer@gmail.com>
2014-07-16 10:02:17 +0200

    remove precise and saucy from netcdf
    The CMake version requirement is now 2.8.12.

--- submit-netcdf.sh
+++ submit-netcdf.sh
@@ -10,7 +10,7 @@ $THIS_DIR/launchpad-submitter \
   --resubmission 1 \
   --source-dir "$HOME/software/netcdf/dev/source/" \
   --debian-dir "$HOME/rcs/debian-netcdf-official/debian/" \
-  --ubuntu-releases precise saucy trusty utopic \
+  --ubuntu-releases trusty utopic \
   --ppas nschloe/netcdf-nightly nschloe/trilinos-nightly \
   --version-getter 'grep "^AC_INIT" configure.ac | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
   --slot 1 \
