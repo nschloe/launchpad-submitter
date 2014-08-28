747cfb6a336f24a40d9800f908f16f2bf10d6308
Nico Schlömer <nico.schloemer@gmail.com>
2014-08-28 23:38:47 +0200

    get trilinos to compile on precise

--- submit-trilinos.sh
+++ submit-trilinos.sh
@@ -3,15 +3,24 @@
 # Set SSH agent variables.
 eval $(cat $HOME/.ssh/agent/info)
 
-THIS_DIR=`dirname $0`
+THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# submit for trusty, utopic
 $THIS_DIR/launchpad-submitter \
   --name trilinos \
   --resubmission 1 \
   --source-dir "$HOME/software/trilinos/github/" \
   --debian-dir "$HOME/rcs/debian-packages/trilinos/debian/" \
-  --ubuntu-releases precise trusty utopic \
+  --ubuntu-releases trusty utopic \
   --version-getter 'grep "Trilinos_VERSION " Version.cmake | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
   --ppas nschloe/trilinos-nightly
-  #--extra-patches-dir patches-trilinos \
-  #--ubuntu-releases precise saucy trusty \
+
+# use a different debian/ folder for precise
+$THIS_DIR/launchpad-submitter \
+  --name trilinos \
+  --resubmission 1 \
+  --source-dir "$HOME/software/trilinos/github/" \
+  --debian-dir "$THIS_DIR/debian-trilinos-precise/" \
+  --ubuntu-releases precise \
+  --version-getter 'grep "Trilinos_VERSION " Version.cmake | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
+  --ppas nschloe/trilinos-nightly
