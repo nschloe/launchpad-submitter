9a77864a83f039cf6dd0a393e9b96aba80e6062a
Nico Schlömer <nico.schloemer@gmail.com>
2015-01-06 17:30:48 +0100

    [trilinos] add separate trusty, utopic debian folders

--- submit-trilinos.sh
+++ submit-trilinos.sh
@@ -15,16 +15,28 @@ $THIS_DIR/launchpad-submitter \
   --patches-blacklist stk-sizet.patch \
   --version-getter 'grep "Trilinos_VERSION " Version.cmake | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
   --ppas nschloe/trilinos-nightly \
-  --submit-hashes-file "$THIS_DIR/trilinos1-submit-hashes.dat"
+  --submit-hashes-file "$THIS_DIR/trilinos0-submit-hashes.dat"
 
 # submit for trusty, utopic
 $THIS_DIR/launchpad-submitter \
   --name trilinos \
   --resubmission 1 \
   --source-dir "$HOME/software/trilinos/github/" \
-  --debian-dir "$HOME/rcs/debian-packages/trilinos/debian/" \
-  --ubuntu-releases trusty utopic vivid \
+  --debian-dir "$THIS_DIR/debian-trilinos-trusty/" \
+  --ubuntu-releases trusty utopic \
   --patches-blacklist stk-sizet.patch \
   --version-getter 'grep "Trilinos_VERSION " Version.cmake | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
   --ppas nschloe/trilinos-nightly \
-  --submit-hashes-file "$THIS_DIR/trilinos0-submit-hashes.dat"
+  --submit-hashes-file "$THIS_DIR/trilinos1-submit-hashes.dat"
+
+# submit for vivid
+$THIS_DIR/launchpad-submitter \
+  --name trilinos \
+  --resubmission 1 \
+  --source-dir "$HOME/software/trilinos/github/" \
+  --debian-dir "$HOME/rcs/debian-packages/trilinos/debian/" \
+  --ubuntu-releases vivid \
+  --patches-blacklist stk-sizet.patch \
+  --version-getter 'grep "Trilinos_VERSION " Version.cmake | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
+  --ppas nschloe/trilinos-nightly \
+  --submit-hashes-file "$THIS_DIR/trilinos2-submit-hashes.dat"
