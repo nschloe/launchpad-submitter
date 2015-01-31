666da01502b875c8dc0160f8f7d73862c4dc3689
Nico Schlömer <nico.schloemer@gmail.com>
2015-01-31 13:31:43 +0100

    [mixxx] remove 9001-waveformsignalcolors_fix.patch

--- submit-mixxx.sh
+++ submit-mixxx.sh
@@ -15,7 +15,6 @@ $THIS_DIR/launchpad-submitter \
     0001-update_configure.patch \
     0004-soundtouch.patch \
     1001-buildsystem.patch \
-    9001-waveformsignalcolors_fix.patch \
   --version-getter 'grep "define VERSION" src/defs_version.h | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
   --ppas nschloe/mixxx-nightly \
   --submit-hashes-file "$THIS_DIR/mixxx-nightly.dat"
