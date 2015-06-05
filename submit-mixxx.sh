bb7b76b83abb29d6ce04cbf399e88eb0aa7aa65f
Nico Schlömer <nico.schloemer@gmail.com>
2015-06-05 20:55:18 +0200

    don't build mixxx for precise

--- submit-mixxx.sh
+++ submit-mixxx.sh
@@ -10,7 +10,7 @@ $THIS_DIR/launchpad-submitter \
   --resubmission 1 \
   --source-dir "$HOME/software/mixxx/pristine/" \
   --debian-dir "$HOME/rcs/debian-packages/mixxx/debian/" \
-  --ubuntu-releases precise trusty utopic vivid wily \
+  --ubuntu-releases trusty utopic vivid wily \
   --patches-blacklist \
     0001-update_configure.patch \
     0004-soundtouch.patch \
