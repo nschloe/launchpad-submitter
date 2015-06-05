58874205e9702079acf5793ad186e632fa022d41
Nico Schlömer <nico.schloemer@gmail.com>
2015-06-06 01:23:37 +0200

    get precise-trilinos back in line

--- submit-trilinos.sh
+++ submit-trilinos.sh
@@ -11,7 +11,7 @@ $THIS_DIR/launchpad-submitter \
   --resubmission 1 \
   --source-dir "$HOME/software/trilinos/github/" \
   --debian-dir "$THIS_DIR/debian-trilinos-precise/" \
-  --ubuntu-releases precise \
+  --ubuntu-releases precise trusty \
   --version-getter 'grep "Trilinos_VERSION " Version.cmake | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
   --ppas nschloe/trilinos-nightly \
   --submit-hashes-file "$THIS_DIR/trilinos0-submit-hashes.dat"
@@ -22,7 +22,7 @@ $THIS_DIR/launchpad-submitter \
   --resubmission 1 \
   --source-dir "$HOME/software/trilinos/github/" \
   --debian-dir "$HOME/rcs/debian-packages/trilinos/debian/" \
-  --ubuntu-releases trusty utopic vivid wily \
+  --ubuntu-releases utopic vivid wily \
   --version-getter 'grep "Trilinos_VERSION " Version.cmake | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
   --ppas nschloe/trilinos-nightly \
   --submit-hashes-file "$THIS_DIR/trilinos1-submit-hashes.dat"
