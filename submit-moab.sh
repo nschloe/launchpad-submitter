0148556eb5e7465ffaf582dbacb3b538830af338
Nico Schlömer <nico.schloemer@gmail.com>
2015-11-04 09:46:09 +0100

    build legacy trilinos releases from current debian/

--- submit-moab.sh
+++ submit-moab.sh
@@ -5,32 +5,32 @@ eval "$(cat "$HOME/.ssh/agent/info")"
 
 THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+#"$THIS_DIR/launchpad-submitter" \
+#  --name moab \
+#  --resubmission 1 \
+#  --source-dir "$HOME/software/moab/source-upstream/" \
+#  --ubuntu-releases trusty vivid \
+#  --debian-prepare 'make' \
+#  --version-getter 'grep AC_INIT configure.ac | sed "s/.*\[MOAB\],\[\([^]]*\)\].*/\1/"' \
+#  --ppas nschloe/moab-nightly \
+#  --submit-hashes-file "$THIS_DIR/moab-submit-hash0.dat" \
+#  "$@"
+#
+## Wait at least a minute to make sure that the below submission gets another
+## timestamp. Otherwise, the upload will be rejected with
+## ```
+## Rejected:
+## File moab_4.8.3pre~201510280949.orig.tar.gz already exists in MOAB nightly,
+## but uploaded version has different contents.
+## ```
+#sleep 60
+
 "$THIS_DIR/launchpad-submitter" \
   --name moab \
   --resubmission 1 \
-  --source-dir "$HOME/software/moab/source-upstream/" \
-  --ubuntu-releases trusty vivid \
-  --debian-prepare 'make' \
-  --version-getter 'grep AC_INIT configure.ac | sed "s/.*\[MOAB\],\[\([^]]*\)\].*/\1/"' \
-  --ppas nschloe/moab-nightly \
-  --submit-hashes-file "$THIS_DIR/moab-submit-hash0.dat" \
-  "$@"
-
-# Wait at least a minute to make sure that the below submission gets another
-# timestamp. Otherwise, the upload will be rejected with
-# ```
-# Rejected:
-# File moab_4.8.3pre~201510280949.orig.tar.gz already exists in MOAB nightly,
-# but uploaded version has different contents.
-# ```
-sleep 60
-
-"$THIS_DIR/launchpad-submitter" \
-  --name moab \
-  --resubmission 1 \
-  --source-dir "$HOME/software/moab/source-upstream/" \
+  --source-dir "$HOME/software/moab/source-nschloe/" \
   --ubuntu-releases wily xenial \
-  --debian-prepare 'ADDITIONAL_DEPS=", libmetis-dev, libparmetis-dev" ADDITIONAL_ENABLES="-DENABLE_METIS:BOOL=ON -DENABLE_PARMETIS:BOOL=ON" make' \
+  --debian-prepare 'ADDITIONAL_DEPS=", libmetis-dev, libparmetis-dev" ADDITIONAL_ENABLES="-DENABLE_METIS:BOOL=ON -DENABLE_PARMETIS:BOOL=ON -DMOAB_BUILD_MBPART:BOOL=ON" make' \
   --version-getter 'grep AC_INIT configure.ac | sed "s/.*\[MOAB\],\[\([^]]*\)\].*/\1/"' \
   --ppas nschloe/moab-nightly \
   --submit-hashes-file "$THIS_DIR/moab-submit-hash1.dat" \
--- submit-trilinos.sh
+++ submit-trilinos.sh
@@ -1,37 +1,43 @@
 #!/bin/sh -ue
 
 # Set SSH agent variables.
-eval $(cat $HOME/.ssh/agent/info)
+eval "$(cat "$HOME/.ssh/agent/info")"
 
 THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Kokkos doesn't support compilers older than GCC 4.7.2 anymore.
-## precise
-#$THIS_DIR/launchpad-submitter \
-#  --name trilinos \
-#  --resubmission 1 \
-#  --source-dir "$HOME/software/trilinos/github/" \
-#  --debian-dir "$THIS_DIR/debian-trilinos-precise/" \
-#  --ubuntu-releases precise \
-#  --version-getter 'grep "Trilinos_VERSION " Version.cmake | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
-#  --ppas nschloe/trilinos-nightly \
-#  --submit-hashes-file "$THIS_DIR/trilinos0-submit-hashes.dat" \
-#  "$@"
+# copy over debian dir and replace respective lines
+rm -rf '/tmp/debian-trusty'
+cp -r "$HOME/rcs/debian-packages/trilinos/debian/" "/tmp/debian-trusty"
+# remove hdf5 support
+sed -i '/libhdf5-openmpi-dev/d' '/tmp/debian-trusty/control'
+sed -i '/HDF5/d' '/tmp/debian-trusty/rules'
+# remove superlu support
+sed -i '/libsuperlu-dev/d' '/tmp/debian-trusty/control'
+sed -i '/SuperLU/d' '/tmp/debian-trusty/rules'
 
 # trusty
-$THIS_DIR/launchpad-submitter \
+"$THIS_DIR/launchpad-submitter" \
   --name trilinos \
   --resubmission 1 \
   --source-dir "$HOME/software/trilinos/github/" \
-  --debian-dir "$THIS_DIR/debian-trilinos-trusty/" \
+  --debian-dir "/tmp/debian-trusty/" \
   --ubuntu-releases trusty \
   --version-getter 'grep "Trilinos_VERSION " Version.cmake | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/"' \
   --ppas nschloe/trilinos-nightly \
   --submit-hashes-file "$THIS_DIR/trilinos1-submit-hashes.dat" \
   "$@"
 
+# Wait at least a minute to make sure that the below submission gets another
+# timestamp. Otherwise, the upload will be rejected with
+# ```
+# Rejected:
+# File moab_4.8.3pre~201510280949.orig.tar.gz already exists in MOAB nightly,
+# but uploaded version has different contents.
+# ```
+sleep 60
+
 # submit for the rest
-$THIS_DIR/launchpad-submitter \
+"$THIS_DIR/launchpad-submitter" \
   --name trilinos \
   --resubmission 1 \
   --source-dir "$HOME/software/trilinos/github/" \
