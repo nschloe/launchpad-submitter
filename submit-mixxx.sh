5a95127ff5740cc549c4137c6706c68951fa07dd
Nico Schlömer <nico.schloemer@gmail.com>
2015-11-15 14:06:38 +0100

    remove patches-blacklist

--- submit-mixxx.sh
+++ submit-mixxx.sh
@@ -8,16 +8,19 @@ THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 SOURCE_DIR="$HOME/software/mixxx/pristine/"
 VERSION=$(grep "define VERSION" "$SOURCE_DIR/src/defs_version.h" | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/")
 
+DEBIAN_PREPARE="
+sed -i \"/0001-update_configure.patch/d\" patches/series; \
+sed -i \"/0004-soundtouch.patch/d\" patches/series; \
+sed -i \"/1001-buildsystem.patch/d\" patches/series; \
+sed -i \"/9001-waveformsignalcolors_fix.patch/d\" patches/series; \
+"
+
 "$THIS_DIR/launchpad-submitter"\
   --name mixxx \
   --source-dir "$SOURCE_DIR" \
   --debian-dir "$HOME/rcs/debian-packages/mixxx/debian/" \
+  --debian-prepare "$DEBIAN_PREPARE" \
   --ubuntu-releases trusty vivid wily xenial \
-  --patches-blacklist \
-    0001-update_configure.patch \
-    0004-soundtouch.patch \
-    1001-buildsystem.patch \
-    9001-waveformsignalcolors_fix.patch \
   --version "$VERSION" \
   --ppas nschloe/mixxx-nightly \
   --submit-hashes-file "$THIS_DIR/mixxx-nightly.dat" \
