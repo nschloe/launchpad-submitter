e39f0d8feb733eb72015decf6a6fa55eec7e626d
Nico Schlömer <nico.schloemer@gmail.com>
2016-05-05 16:49:46 +0200

    fix bug in reupload checker

--- submit-docker-gc.sh
+++ submit-docker-gc.sh
@@ -11,19 +11,9 @@ cd "$SOURCE_DIR" && git pull
 VERSION=$(cat "$SOURCE_DIR/version.txt")
 FULL_VERSION="$VERSION~$(date +"%Y%m%d%H%M%S")"
 
-HASH=$(git show --pretty=format:'%T')
-HASHFILE="$THIS_DIR/docker-gc-submit-hash.dat"
-if [ "$HASH" = "$(cat "$HASHFILE")" ]; then
-  echo "Already submitted."
-  exit 1
-fi
-
 "$THIS_DIR/launchpad-submit" \
   --directory "$SOURCE_DIR" \
   --ubuntu-releases trusty wily xenial yakkety \
   --version "$FULL_VERSION" \
-  --ppas nschloe/docker-gc-nightly \
+  --ppa nschloe/docker-gc-nightly \
   "$@"
-
-# Update hash
-echo "$HASH" > "$HASHFILE"
