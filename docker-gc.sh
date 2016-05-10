ab9c17253868502cf542044923d1cfa11e2307cd
Nico Schlömer <nico.schloemer@gmail.com>
2016-05-10 12:59:44 +0200

    update docker-gc

--- docker-gc.sh
+++ docker-gc.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -ue
 
-# Set SSH agent variables.
 . "$HOME/.keychain/$(/bin/hostname)-sh"
+
+THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 SOURCE_DIR="$HOME/software/docker-gc/upstream/"
 cd "$SOURCE_DIR" && git pull
@@ -9,12 +10,12 @@ cd "$SOURCE_DIR" && git pull
 VERSION=$(cat "$SOURCE_DIR/version.txt")
 FULL_VERSION="$VERSION~$(date +"%Y%m%d%H%M%S")"
 
-THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 "$HOME/rcs/launchpad-tools/launchpad-submit" \
   --directory "$SOURCE_DIR" \
   --ubuntu-releases trusty wily xenial yakkety \
   --version "$FULL_VERSION" \
   --ppa nschloe/docker-gc-nightly \
   --debuild-params="-p$THIS_DIR/mygpg" \
-  --submit-id "Nico Schlömer <nico.schloemer@gmail.com>" \
+  --debfullname "Nico Schlömer" \
+  --debemail "nico.schloemer@gmail.com" \
   "$@"
