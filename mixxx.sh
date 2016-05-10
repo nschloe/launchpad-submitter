614774bf1c028e87aaf49002f8e9753735d927fd
Nico Schlömer <nico.schloemer@gmail.com>
2016-05-10 12:58:06 +0200

    use new git spec in mixxx

--- mixxx.sh
+++ mixxx.sh
@@ -5,14 +5,9 @@
 
 THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-SOURCE_DIR="$HOME/software/mixxx/pristine/"
-cd "$SOURCE_DIR" && git pull
-DEBIAN_DIR="$HOME/rcs/debian-packages/mixxx/debian/"
-cd "$DEBIAN_DIR" && git pull
-
 DIR=$("$HOME/rcs/launchpad-tools/create-debian-repo" \
-  --source "$SOURCE_DIR" \
-  --debian "$DEBIAN_DIR"
+   --source "git@github.com:mixxxdj/mixxx.git" \
+   --debian "git://anonscm.debian.org/git/pkg-multimedia/mixxx.git"
   )
 
 VERSION=$(grep "define VERSION" "$DIR/src/defs_version.h" | sed "s/[^0-9]*\([0-9][\.0-9]*\).*/\1/")
@@ -24,7 +19,7 @@ sed -i "/0006-opengles.patch/d" "$DIR/debian/patches/ubuntu.series"
 sed -i "/1001-buildsystem.patch/d" "$DIR/debian/patches/ubuntu.series"
 sed -i "s/libsoundtouch-dev (>= 1.8.0)/libsoundtouch-dev (>= 1.7.1)/g" "$DIR/debian/control"
 sed -i "s/scons,/scons, libupower-glib-dev,/g" "$DIR/debian/control"
-cd "$DIR" && git commit -a -m "some changes"
+cd "$DIR" && git commit -a -m "update patches"
 
 "$HOME/rcs/launchpad-tools/launchpad-submit" \
   --directory "$DIR" \
