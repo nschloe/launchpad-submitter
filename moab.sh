1f844c8b0a268ba08d9d65d36c908ca64de9230d
Nico Schlömer <nico.schloemer@gmail.com>
2016-05-12 10:30:35 +0200

    simplify moab submitter

--- moab.sh
+++ moab.sh
@@ -14,24 +14,6 @@ echo "Working directory: $DIR"
 VERSION=$(grep AC_INIT "$DIR/configure.ac" | sed "s/.*\[MOAB\],\[\([^]]*\)\].*/\1/")
 FULL_VERSION="$VERSION~$(date +"%Y%m%d%H%M%S")"
 
-# # A metis bug prevents us for using that pre-wily.
-# cd "$DIR/debian"
-# sed -i "/libmetis-dev/d" control
-# sed -i "/libparmetis-dev/d" control
-# sed -i "/ENABLE_METIS/d" rules
-# sed -i "/ENABLE_PARMETIS/d" rules
-# sed -i "/MOAB_BUILD_MBPART/d" rules
-# 
-# "$THIS_DIR/launchpad-submit" \
-#   --orig-dir "$SOURCE_DIR" \
-#   --debian-dir "$DEBIAN_DIR" \
-#   --ubuntu-releases trusty \
-#   --version-override "$FULL_VERSION" \
-#   --ppa nschloe/moab-nightly \
-#   "$@"
-
-FULL_VERSION="$VERSION~$(date +"%Y%m%d%H%M%S")"
-
 "$HOME/rcs/launchpadtools/tools/launchpad-submit" \
   --directory "$DIR" \
   --ubuntu-releases trusty wily xenial yakkety \
