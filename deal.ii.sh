047a4d69fc7608e1580dad8253213fb0f70ec540
Nico Schlömer <nico.schloemer@gmail.com>
2016-05-27 11:11:09 +0200

    some small fixes

--- deal.ii.sh
+++ deal.ii.sh
@@ -16,12 +16,13 @@ clone \
    "$DEBIAN_DIR"
 
 sed -i '/doc\/doxygen\/deal.II\/images/d' "$DEBIAN_DIR/debian/rules"
+sed -i '/getElementById/,+2 d' "$DEBIAN_DIR/debian/rules"
 
 launchpad-submit \
   --orig "$ORIG_DIR" \
   --debian "$DEBIAN_DIR/debian" \
   --update-patches \
-  --ubuntu-releases wily xenial yakkety \
+  --ubuntu-releases yakkety \
   --version-override "$UPSTREAM_VERSION~$(date +"%Y%m%d%H%M%S")" \
   --version-append-hash \
   --ppa nschloe/deal.ii-nightly \
