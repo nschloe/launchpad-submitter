4a74ab129bb382b8adfc927fbfb516e1d6f552c8
Nico Schlömer <nico.schloemer@gmail.com>
2016-05-28 16:54:41 +0200

    add sympy

---
+++ sympy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -ue
+
+THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+ORIG_DIR=$(mktemp -d)
+clone "https://github.com/sympy/sympy.git" "$ORIG_DIR"
+
+UPSTREAM_VERSION=$(sed 's/[^\"]*\"\([^\"]*\)\".*/\1/' "$ORIG_DIR/sympy/release.py")
+
+DEBIAN_DIR=$(mktemp -d)
+clone \
+   "git://anonscm.debian.org/git/debian-science/packages/sympy.git" \
+   "$DEBIAN_DIR"
+
+launchpad-submit \
+  --orig "$ORIG_DIR" \
+  --debian "$DEBIAN_DIR/debian" \
+  --ubuntu-releases trusty wily xenial yakkety \
+  --version-override "$UPSTREAM_VERSION~$(date +"%Y%m%d%H%M%S")" \
+  --version-append-hash \
+  --update-patches \
+  --ppa nschloe/sympy-nightly \
+  --debuild-params="-p$THIS_DIR/mygpg"
+
+rm -rf "$ORIG_DIR" "$DEBIAN_DIR"
