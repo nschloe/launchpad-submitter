fefde8435cdf762da17a01d62002ffaf5effe22f
Nico Schlömer <nico.schloemer@gmail.com>
2016-05-28 12:13:44 +0200

    add lapack

---
+++ lapack.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -ue
+
+THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+ORIG_DIR=$(mktemp -d)
+clone "https://icl.cs.utk.edu/svn/lapack-dev/lapack/trunk" "$ORIG_DIR"
+
+MAJOR=$(grep 'set(LAPACK_MAJOR_VERSION ' "$ORIG_DIR/CMakeLists.txt" | sed 's/^[^0-9]*\([0-9]*\).*/\1/')
+MINOR=$(grep 'set(LAPACK_MINOR_VERSION ' "$ORIG_DIR/CMakeLists.txt" | sed 's/^[^0-9]*\([0-9]*\).*/\1/')
+PATCH=$(grep 'set(LAPACK_PATCH_VERSION ' "$ORIG_DIR/CMakeLists.txt" | sed 's/^[^0-9]*\([0-9]*\).*/\1/')
+UPSTREAM_VERSION="$MAJOR.$MINOR.$PATCH~$(date +"%Y%m%d%H%M%S")"
+
+DEBIAN_DIR=$(mktemp -d)
+clone \
+   "git://anonscm.debian.org/git/debian-science/packages/lapack.git" \
+   "$DEBIAN_DIR"
+
+launchpad-submit \
+  --orig "$ORIG_DIR" \
+  --debian "$DEBIAN_DIR/debian" \
+  --ubuntu-releases trusty wily xenial yakkety \
+  --version-override "$UPSTREAM_VERSION" \
+  --version-append-hash \
+  --update-patches \
+  --ppa nschloe/lapack-nightly \
+  --debuild-params="-p$THIS_DIR/mygpg"
+
+rm -rf "$ORIG_DIR" "$DEBIAN_DIR"
