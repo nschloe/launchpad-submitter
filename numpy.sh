f7a5889f6879a0d569699f073ea3f1280eb3deeb
Nico Schlömer <nico.schloemer@gmail.com>
2016-05-29 19:17:47 +0200

    add numpy

---
+++ numpy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -ue
+
+THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+ORIG_DIR=$(mktemp -d)
+clone "https://github.com/numpy/numpy.git" "$ORIG_DIR"
+
+MAJOR=$(grep 'MAJOR' "$ORIG_DIR/setup.py" | sed 's/^[^0-9]*\([0-9]*\).*/\1/')
+MINOR=$(grep 'MINOR' "$ORIG_DIR/setup.py" | sed 's/^[^0-9]*\([0-9]*\).*/\1/')
+PATCH=$(grep 'MICRO' "$ORIG_DIR/setup.py" | sed 's/^[^0-9]*\([0-9]*\).*/\1/')
+UPSTREAM_VERSION="$MAJOR.$MINOR.$PATCH"
+
+DEBIAN_DIR=$(mktemp -d)
+clone \
+   "git://anonscm.debian.org/git/python-modules/packages/python-numpy.git" \
+   "$DEBIAN_DIR"
+
+launchpad-submit \
+  --orig "$ORIG_DIR" \
+  --debian "$DEBIAN_DIR/debian" \
+  --ubuntu-releases trusty wily xenial yakkety \
+  --version-override "$UPSTREAM_VERSION~$(date +"%Y%m%d%H%M%S")" \
+  --version-append-hash \
+  --update-patches \
+  --ppa nschloe/numpy-nightly \
+  --debuild-params="-p$THIS_DIR/mygpg"
+
+rm -rf "$ORIG_DIR" "$DEBIAN_DIR"
