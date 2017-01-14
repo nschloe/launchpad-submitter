1247b83cbf66bb0b103aad9565baa29fb3c25749
Nico Schlömer <nico.schloemer@gmail.com>
2017-01-14 14:03:20 +0100

    fixes for mikado

--- mikado.sh
+++ mikado.sh
@@ -6,16 +6,16 @@ TMP_DIR=$(mktemp -d)
 cleanup() { rm -rf "$TMP_DIR"; }
 trap cleanup EXIT
 
-DIR="$TMP_DIR"
-clone --ignore-hidden "https://github.com/nschloe/mikado.git" "$DIR"
+ORIG_DIR="$TMP_DIR/orig"
+clone --ignore-hidden "https://github.com/nschloe/mikado.git" "$ORIG_DIR"
 
-MAJOR=$(grep "set(Mikado_MAJOR_VERSION " "$DIR/CMakeLists.txt" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
-MINOR=$(grep "set(Mikado_MINOR_VERSION " "$DIR/CMakeLists.txt" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
-PATCH=$(grep "set(Mikado_PATCH_VERSION " "$DIR/CMakeLists.txt" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
+MAJOR=$(grep "set(Mikado_MAJOR_VERSION " "$ORIG_DIR/CMakeLists.txt" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
+MINOR=$(grep "set(Mikado_MINOR_VERSION " "$ORIG_DIR/CMakeLists.txt" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
+PATCH=$(grep "set(Mikado_PATCH_VERSION " "$ORIG_DIR/CMakeLists.txt" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
 VERSION="$MAJOR.$MINOR.$PATCH"
 
 launchpad-submit \
-  --work-dir "$DIR" \
+  --work-dir "$TMP_DIR" \
   --ubuntu-releases trusty xenial yakkety zesty \
   --version-override "$VERSION~$(date +"%Y%m%d%H%M%S")" \
   --version-append-hash \
