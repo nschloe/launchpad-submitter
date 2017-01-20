f0672c37f062d9d90688059a5bd5a2af5489b921
Nico Schlömer <nico.schloemer@gmail.com>
2017-01-20 22:57:10 +0100

    remove obsolete metis backport script

--- backports/metis-backport.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -ue
-
-"$HOME/rcs/launchpadtools/tools/launchpad-backport" \
-  --orig "http://http.debian.net/debian/pool/main/m/metis/metis_5.1.0.dfsg-4.dsc" \
-  --ubuntu-releases trusty \
-  --ppa nschloe/metis-backports
