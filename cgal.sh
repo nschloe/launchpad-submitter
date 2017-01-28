2996477e2092320caaa4cca7e6169cc1e51300ca
Nico Schlömer <nico.schloemer@gmail.com>
2017-01-28 14:52:08 +0100

    fixes for cgal

--- cgal.sh
+++ cgal.sh
@@ -6,30 +6,28 @@ TMP_DIR=$(mktemp -d)
 cleanup() { rm -rf "$TMP_DIR"; }
 trap cleanup EXIT
 
-ORIG_DIR="$TMP_DIR/orig"
+CLONE_DIR="$TMP_DIR/clone"
 CACHE="$HOME/.cache/repo/cgal"
 git -C "$CACHE" pull || git clone "https://github.com/CGAL/cgal.git" "$CACHE"
-git clone --shared "$CACHE" "$ORIG_DIR"
+git clone --shared "$CACHE" "$CLONE_DIR"
 
 # Create the release dir
 cd "$TMP_DIR"
-"$ORIG_DIR/Scripts/developer_scripts/create_new_release" "$ORIG_DIR" --verbose
+"$CLONE_DIR/Scripts/developer_scripts/create_new_release" "$CLONE_DIR"
 cd "$TMP_DIR/tmp/"
 tar xf CGAL-last.tar.gz
 TARFILE=$(cat "$TMP_DIR/tmp/LATEST")
 DIRECTORY="$TMP_DIR/tmp/${TARFILE%.tar.gz}"
-if [ ! -d "$DIRECTORY" ]; then
-  echo "Couldn't find directory $DIRECTORY."
-  exit 1
-fi
-ORIG_DIR="$DIRECTORY"
+ORIG_DIR="$TMP_DIR/orig"
+mv "$DIRECTORY" "$ORIG_DIR"
 
 VERSION=$(cat "$ORIG_DIR/VERSION")
 FULL_VERSION="$VERSION~$(date +"%Y%m%d%H%M%S")"
 
-DEBIAN_DIR="$TMP_DIR/orig/debian"
+DEBIAN_DIR="$ORIG_DIR/debian"
 CACHE="$HOME/.cache/repo/cgal-debian"
 git -C "$CACHE" pull || git clone "https://github.com/nschloe/cgal-debian.git" "$CACHE"
+echo "rsync -a \"$CACHE/\" \"$ORIG_DIR/debian\""
 rsync -a "$CACHE/debian" "$ORIG_DIR"
 
 cd "$DEBIAN_DIR"
