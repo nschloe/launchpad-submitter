6c73c87cc7fc20d524cf46e80ea75bc587bd6a2e
Nico Schlömer <nico.schloemer@gmail.com>
2017-01-28 14:49:49 +0100

    fix for scipy

--- scipy.sh
+++ scipy.sh
@@ -16,7 +16,7 @@ MINOR=$(grep 'MINOR' "$ORIG_DIR/setup.py" | sed 's/^[^0-9]*\([0-9]*\).*/\1/')
 PATCH=$(grep 'MICRO' "$ORIG_DIR/setup.py" | sed 's/^[^0-9]*\([0-9]*\).*/\1/')
 UPSTREAM_VERSION="$MAJOR.$MINOR.$PATCH"
 
-CACHE="$HOME/.cache/repo/petsc-debian"
+CACHE="$HOME/.cache/repo/scipy-debian"
 git -C "$CACHE" pull || git clone "git://anonscm.debian.org/git/python-modules/packages/python-scipy.git" "$CACHE"
 rsync -a "$CACHE/debian" "$ORIG_DIR"
 
