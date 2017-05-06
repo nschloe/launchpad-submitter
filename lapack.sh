6ccb07ff5d13f53c4c7682db2a5849aa37659ea3
Nico Schlömer <nico.schloemer@gmail.com>
2017-05-06 20:35:53 +0200

    fix for lapack

--- lapack.sh
+++ lapack.sh
@@ -20,6 +20,9 @@ CACHE="$HOME/.cache/repo/lapack-debian"
 git -C "$CACHE" pull || git clone "https://anonscm.debian.org/git/debian-science/packages/lapack.git" "$CACHE"
 rsync -a "$CACHE/debian" "$ORIG_DIR"
 
+DEBIAN_DIR="$ORIG_DIR/debian"
+sed -i '/cleanall/d' "$DEBIAN_DIR/rules"
+
 launchpad-submit \
   --work-dir "$TMP_DIR" \
   --ubuntu-releases yakkety zesty artful \
