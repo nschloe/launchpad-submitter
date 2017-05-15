660813a76551576df487206c0a65e970b95889ed
Nico Schlömer <nico.schloemer@gmail.com>
2017-05-15 19:12:08 +0200

    get mshr ready for python3

--- fenics/mshr.sh
+++ fenics/mshr.sh
@@ -14,15 +14,31 @@ git clone --shared "$CACHE" "$ORIG_DIR"
 MAJOR=$(grep 'MSHR_VERSION_MAJOR ' "$ORIG_DIR/CMakeLists.txt" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
 MINOR=$(grep 'MSHR_VERSION_MINOR ' "$ORIG_DIR/CMakeLists.txt" | sed 's/.*\([0-9]\).*/\1/')
 MICRO=$(grep 'MSHR_VERSION_MICRO ' "$ORIG_DIR/CMakeLists.txt" | sed 's/.*\([0-9]\).*/\1/')
-FULL_VERSION="$MAJOR.$MINOR.$MICRO~git$(date +"%Y%m%d")"
+FULL_VERSION="$MAJOR.$MINOR.$MICRO~git$(date +"%Y%m%d%H%M")"
 
 DEBIAN_DIR="$TMP_DIR/orig/debian"
 CACHE="$HOME/.cache/repo/mshr-debian"
 git -C "$CACHE" pull || git clone "https://anonscm.debian.org/git/debian-science/packages/fenics/mshr.git" "$CACHE"
 rsync -a "$CACHE/debian" "$ORIG_DIR"
 
-sed -i 's/Build-Depends:/Build-Depends: python-petsc4py, python-slepc4py,/g' "$ORIG_DIR/debian/control"
+DEBIAN_VERSION=$(head -n1 "$ORIG_DIR/debian/changelog" | sed 's/[^0-9]*\([0-9]*\.[0-9]\).*/\1/')
+sed -i "s/libdolfin$DEBIAN_VERSION/libdolfin$MAJOR.$MINOR/g" "$ORIG_DIR/debian/control"
 
+cd "$ORIG_DIR/debian"
+rename "s/$DEBIAN_VERSION/$MAJOR.$MINOR/" ./*
+rename "s/python-/python3-/" ./*
+
+sed -i 's/Build-Depends:/Build-Depends: python-petsc4py, python-slepc4py,/g' "$ORIG_DIR/debian/control"
+sed -i '/X-Python-Version: >= 2.5/a X-Python3-Version: >= 3.4' "$ORIG_DIR/debian/control"
+#
+sed -i 's/python-ffc/python3-ffc/g' "$ORIG_DIR/debian/control"
+sed -i 's/python-numpy/python3-numpy/g' "$ORIG_DIR/debian/control"
+sed -i 's/python-mshr/python3-mshr/g' "$ORIG_DIR/debian/control"
+sed -i 's/python-dolfin/python3-dolfin/g' "$ORIG_DIR/debian/control"
+#
+sed -i 's/--with python2/--with python3/g' "$ORIG_DIR/debian/rules"
+sed -i 's/pyversions/py3versions/g' "$ORIG_DIR/debian/rules"
+#
 sed -i "/mshrable/d" "$DEBIAN_DIR/libmshr-dev.install"
 
 launchpad-submit \
