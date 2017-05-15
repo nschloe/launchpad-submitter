2fa673db043c8b104a8896ffed8dd371cabddbdb
Nico Schlömer <nico.schloemer@gmail.com>
2017-05-15 19:13:33 +0200

    get dolfin python3-ready

--- fenics/dolfin.sh
+++ fenics/dolfin.sh
@@ -22,30 +22,35 @@ git -C "$CACHE" pull || git clone "https://anonscm.debian.org/git/debian-science
 rsync -a "$CACHE/debian" "$ORIG_DIR"
 
 # remove overly strict conditions
-sed -i 's/python-ffc (<< 2016.3.0),//g' "$ORIG_DIR/debian/control"
-sed -i 's/python-dijitso (<< 2016.3.0),//g' "$ORIG_DIR/debian/control"
+sed -i 's/python3-ffc (<< 2016.3.0),//g' "$ORIG_DIR/debian/control"
+sed -i 's/python3-dijitso (<< 2016.3.0),//g' "$ORIG_DIR/debian/control"
 
-sed -i 's/python-ffc (>= ${source:Upstream-Version}), python-ffc (<< ${source:Next-Upstream-Version})/python-ffc/g' "$ORIG_DIR/debian/control"
-sed -i 's/python-dijitso (>= ${source:Upstream-Version}), python-dijitso (<< ${source:Next-Upstream-Version})/python-dijitso/g' "$ORIG_DIR/debian/control"
-sed -i 's/python-instant (>= ${source:Upstream-Version}), python-instant (<< ${source:Next-Upstream-Version})/python-instant/g' "$ORIG_DIR/debian/control"
-sed -i 's/python-ufl (>= ${source:Upstream-Version}), python-ufl (<< ${source:Next-Upstream-Version})/python-ufl/g' "$ORIG_DIR/debian/control"
+# vtk7
+sed -i 's/libvtk6/libvtk7/g' "$ORIG_DIR/debian/control"
+
+sed -i 's/python3-ffc (>= ${source:Upstream-Version}), python3-ffc (<< ${source:Next-Upstream-Version})/python3-ffc/g' "$ORIG_DIR/debian/control"
+sed -i 's/python3-dijitso (>= ${source:Upstream-Version}), python3-dijitso (<< ${source:Next-Upstream-Version})/python3-dijitso/g' "$ORIG_DIR/debian/control"
+sed -i 's/python3-instant (>= ${source:Upstream-Version}), python3-instant (<< ${source:Next-Upstream-Version})/python3-instant/g' "$ORIG_DIR/debian/control"
+sed -i 's/python3-ufl (>= ${source:Upstream-Version}), python3-ufl (<< ${source:Next-Upstream-Version})/python3-ufl/g' "$ORIG_DIR/debian/control"
 
 # sed -i 's/python-dev,/python-dev, python3-dev, python3, python3-minimal,/g' "$ORIG_DIR/debian/control"
 # sed -i 's/--with python2/--with python3/g' "$ORIG_DIR/debian/rules"
-sed -i 's/-D CMAKE_SKIP_RPATH:BOOL=ON/-D CMAKE_SKIP_RPATH:BOOL=ON -DDOLFIN_USE_PYTHON3:BOOL=OFF/g' "$ORIG_DIR/debian/rules"
+# sed -i 's/-D CMAKE_SKIP_RPATH:BOOL=ON/-D CMAKE_SKIP_RPATH:BOOL=ON -DDOLFIN_USE_PYTHON3:BOOL=OFF/g' "$ORIG_DIR/debian/rules"
 
 DEBIAN_VERSION=$(head -n1 ~/.cache/repo/dolfin-debian/debian/changelog | sed 's/[^0-9]*\([0-9]*\.[0-9]\).*/\1/')
 sed -i "s/libdolfin$DEBIAN_VERSION/libdolfin$MAJOR.$MINOR/g" "$ORIG_DIR/debian/control"
 
 cd "$ORIG_DIR/debian"
 rename "s/$DEBIAN_VERSION/$MAJOR.$MINOR/" ./*
+mv python-dolfin.install python3-dolfin.install
 
 # No xenial:
 # Missing build dependencies: python-slepc4py
+#  --ubuntu-releases yakkety zesty artful \
 launchpad-submit \
   --work-dir "$TMP_DIR" \
   --update-patches \
-  --ubuntu-releases yakkety zesty artful \
+  --ubuntu-releases zesty \
   --version-override "$FULL_VERSION" \
   --version-append-hash \
   --ppa nschloe/fenics-nightly \
