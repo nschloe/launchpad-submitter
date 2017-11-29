dd405ecb9ba337b08b5f6849f54e22cacebcb6ef
Nico Schlömer <nico.schloemer@gmail.com>
2017-11-29 12:52:40 +0100

    another vtk7 fix

--- vtk7.sh
+++ vtk7.sh
@@ -40,7 +40,9 @@ sed -i "/50_use_system_utf8.patch/d" "$ORIG_DIR/debian/patches/series"
 
 #
 sed -i "s/vtk-$DEBIAN_MAJOR.$DEBIAN_MINOR/vtk-$MAJOR.$MINOR/" "$ORIG_DIR/debian/rules"
+sed -i "s/vtk-$DEBIAN_MAJOR.$DEBIAN_MINOR/vtk-$MAJOR.$MINOR/" "$ORIG_DIR/debian/libvtk$DEBIAN_MAJOR-dev.install"
 sed -i "/vtk_netcdfcpp.h/d" "$ORIG_DIR/debian/rules"
+sed -i "/vtk_netcdfcpp.h/d" "$ORIG_DIR/debian/libvtk$DEBIAN_MAJOR-dev.install"
 
 
 launchpad-submit \
